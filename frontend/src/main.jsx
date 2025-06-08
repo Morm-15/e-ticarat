0181ad01fe92bf78969fd1cda20d7e75fe123532
Morm-15 <mostafaradwanyos@gmail.com>
2025-06-08 19:32:12 +0300

    add vercel.json

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import './i18n'; // مهم جداً
 
 // الخطوتين الجديدتين لإضافة React Query:
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {BrowserRouter} from "react-router-dom";
 
 // إنشاء عميل React Query
 const queryClient = new QueryClient();
@@ -15,9 +14,7 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <QueryClientProvider client={queryClient}>
-            <BrowserRouter>
                 <App/>
-            </BrowserRouter>
         </QueryClientProvider>
     </React.StrictMode>
 );
